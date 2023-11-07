64171b4f6e6e73233af49d54314d30fda083c02c
kharo1963 <kharo1963@gmail.com>
2023-11-07 17:12:43 +0300

    yandexcloud

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  interpretatorURL : 'http://localhost:8080/',
+  interpretatorURL : 'https://bbaqndbutut7tp2bjrh3.containers.yandexcloud.net/',
+  //interpretatorURL : 'http://localhost:8080/',
+  //interpretatorURL : 'https://bootiml-production.up.railway.app/',
   sourceTextURL    : 'assets/source-text/m-ext-gcd.txt'
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  interpretatorURL : 'https://bootiml-production.up.railway.app/',
+  interpretatorURL : 'https://bbaqndbutut7tp2bjrh3.containers.yandexcloud.net/',
+  //interpretatorURL : 'https://bootiml-production.up.railway.app/',
   sourceTextURL    : 'assets/source-text/m-ext-gcd.txt'
 };
