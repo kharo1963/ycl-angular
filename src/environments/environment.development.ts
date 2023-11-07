1c3394d1d4bc0559f6e81dae7fe28877261c641e
kharo1963 <kharo1963@gmail.com>
2023-11-07 17:39:57 +0300

    yandexcloud

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
   interpretatorURL : 'https://bbaqndbutut7tp2bjrh3.containers.yandexcloud.net/',
   //interpretatorURL : 'http://localhost:8080/',
-  //interpretatorURL : 'https://bootiml-production.up.railway.app/',
   sourceTextURL    : 'assets/source-text/m-ext-gcd.txt'
 };
 
